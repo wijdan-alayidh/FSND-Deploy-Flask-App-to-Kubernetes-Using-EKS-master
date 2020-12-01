ea548d3b64692b3a3934adcd81fdbe19cbadb547
wijdan56 <wijdan_alayidh@hotmail.com>
2020-12-01 17:12:25 +0300

    add assert false into tests in test_main file

--- test_main.py
+++ test_main.py
@@ -27,6 +27,8 @@ def test_health(client):
     assert response.status_code == 200
     assert response.json == 'Healthy'
 
+    assert False
+
 
 def test_auth(client):
     body = {'email': EMAIL,
@@ -38,3 +40,5 @@ def test_auth(client):
     assert response.status_code == 200
     token = response.json['token']
     assert token is not None
+
+    assert False
